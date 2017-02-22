e771d11d049f0519cc7334e9b3f2302bdad4b21d
tema_on <tema_on@ukr.net>
2017-02-22 23:36:57 +0200

    fix

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tema_on
+ * Date: 22.02.17
+ * Time: 21:46
+ */
+
+$connect = mysqli_connect('localhost', 'root', 'зфыыцщкв');
+
+var_dump($connect);
